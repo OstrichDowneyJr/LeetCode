5e15ef8f64a299482353ac29491546bf27fdddb4
OstrichDowneyJr <25938528+OstrichDowneyJr@users.noreply.github.com>
2024-04-01 18:03:34 +0200

    rozbudowanie o funkcje na listach

--- LinkedList/LinkedList.c
+++ LinkedList/LinkedList.c
@@ -7,11 +7,55 @@
      struct node* next;
  }node_t;
  
-struct node_t* mergeTwoLists(node_t* list1, node_t* list2) { 
-    
-    node_t* root = (node_t*)malloc(sizeof(node_t));
+ void print_list_node(node_t* head) {
+
+     node_t* current = head;
+     while (current != NULL) {
+         printf("%d\n", current->val);
+         current = current->next;
+     }
+ }
+
+ void push_node(node_t* head, int val) {
+     node_t* current = head;
+     while (current->next != NULL) {
+         current = current->next;
+     }
+     current->next = (node_t*)malloc(sizeof(node_t));
+     current->next->val = val;
+     current->next->next = NULL;
+ }
+
+ void init_node(node_t* head,int size, int* val) {
+     node_t* current = head;
+     while (current->next != NULL) {
+         current = current->next;
+     }
+
+     current->next = (node_t*)malloc(sizeof(node_t));
+     current->next->val = val;
+     current->next->next = NULL;
+ }
+
+ void push_at_beginning(node_t** head, int val) {
+
+     node_t* new_node;
+     new_node = (node_t*)malloc(sizeof(node_t));
+     new_node->val = val;
+     new_node->next = *head;
+     *head = new_node;
+ }
+
+ 
+ node_t* mergeTwoLists(node_t* list1, node_t* list2) { 
+   
+    node_t* root = NULL;
+    root = (node_t*)malloc(sizeof(node_t));
+    if (root == NULL)
+        return 1;
     root->val = 0;
     root->next = NULL;
+
 
     node_t* iter = root;
 
@@ -63,30 +107,13 @@ struct node_t* mergeTwoLists(node_t* list1, node_t* list2) {
 
 int main(int argc, char** argv) {
 
-     node_t* l1 = (node_t*)malloc(sizeof(node_t));
-     node_t* l2 = (node_t*)malloc(sizeof(node_t));
-
-    l1->val = 0;
-    l1->next = (node_t*)malloc(sizeof(node_t));
-    l1 = l1->next;
+    node_t* l1 = (node_t*)malloc (sizeof(node_t));
     l1->val = 1;
-    l1->next = (node_t*)malloc(sizeof(node_t));
-    l1 = l1->next;
-    l1->val = 4;
     l1->next = NULL;
-
-    l2->val = 0;
-    l2->next = (node_t*)malloc(sizeof(node_t));
-    l2 = l2->next;
-    l2->val = 3;
-    l2->next = (node_t*)malloc(sizeof (node_t));
-    l2 = l2->next;
-    l2->val = 2;
-    l2->next = NULL;
-
-    mergeTwoLists(l1, l2);
-
-
+    push_node(l1, 3);
+    print_list_node(l1);
+    push_node(l1, 30);
+    print_list_node(l1);
 
 	return 0;
 }

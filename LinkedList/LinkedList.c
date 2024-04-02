75888a3ea0ae15a9c39ef05fdf73bd5d61e0d532
OstrichDowneyJr <25938528+OstrichDowneyJr@users.noreply.github.com>
2024-04-02 14:20:26 +0200

    IT WORKED! IT DID WORK!

--- LinkedList/LinkedList.c
+++ LinkedList/LinkedList.c
@@ -16,102 +16,151 @@
      }
  }
 
- void push_node(node_t* head, int val) {
-     node_t* current = head;
-     while (current->next != NULL) {
-         current = current->next;
+ void push_node(node_t** head, int val) {
+
+     node_t* new_node = malloc(sizeof(node_t));
+     if (new_node == NULL) {
+         fprintf(stderr, "Memory allocation failed\n");
+         exit(EXIT_FAILURE);
      }
-     current->next = (node_t*)malloc(sizeof(node_t));
-     current->next->val = val;
-     current->next->next = NULL;
+
+     new_node->val = val;
+     new_node->next = NULL;
+
+     if (*head == NULL) {
+         *head = new_node;
+     }
+     else {
+         node_t* current = *head;
+         while (current->next != NULL) {
+             current = current->next;
+         }
+         current->next = new_node;
+     }
  }
 
+
  node_t* init_node(int size, int* numb) {
-     node_t* current = NULL; 
-     current = (node_t*)malloc(sizeof(node_t));
-     for (int i = 0;i < size-1;i++) {
-         current->val = numb[i]; // Why its dereferencing NULL pointer?  
-         current->next= (node_t*)malloc(sizeof(node_t));
+     if (size <= 0 || numb == NULL) {
+         fprintf(stderr, "Invalid input\n");
+         exit(EXIT_FAILURE);
+     }
+
+     node_t* head = malloc(sizeof(node_t));
+     if (head == NULL) {
+         fprintf(stderr, "Memory allocation failed\n");
+         exit(EXIT_FAILURE);
+     }
+     node_t* current = head;
+     for (int i = 0; i < size - 1; i++) {
+         current->val = numb[i];
+         current->next = malloc(sizeof(node_t));
+         if (current->next == NULL) {
+             fprintf(stderr, "Memory allocation failed\n");
+             exit(EXIT_FAILURE);
+         }
          current = current->next;
-     } 
-     return current;
+     }
+     // Assign value to the last node
+     current->val = numb[size - 1];
+     current->next = NULL;
+     return head;
  }
 
  void push_at_beginning(node_t** head, int val) {
-//TODO function not working correctly
-     node_t* new_node;
-     new_node = (node_t*)malloc(sizeof(node_t));
+     node_t* new_node = malloc(sizeof(node_t));
+     if (new_node == NULL) {
+         fprintf(stderr, "Memory allocation failed\n");
+         exit(EXIT_FAILURE);
+     }
      new_node->val = val;
-     new_node->next = *head;
+	 new_node->next = *head;
      *head = new_node;
  }
 
  
- node_t* mergeTwoLists(node_t* list1, node_t* list2) { 
-   
-    node_t* root = NULL;
-    root = (node_t*)malloc(sizeof(node_t));
-    if (root == NULL)
-        return 1;
-    root->val = 0;
-    root->next = NULL;
+ node_t* mergeTwoLists(node_t* list1, node_t* list2) {
+     node_t* root = (node_t*)malloc(sizeof(node_t));
+     if (root == NULL)
+         return NULL; 
+     root->val = 0;
+     root->next = NULL;
 
+     node_t* iter = root;
 
-    node_t* iter = root;
+     if (list1 == NULL)
+         return list2;
+     if (list2 == NULL)
+         return list1;
 
-    if (list1 == NULL) 
-        return list2;
-    if (list2 == NULL) 
-        return list1;
-    if (list1 == NULL && list2 == NULL) 
-        return NULL;
+     if (list1 == NULL && list2 == NULL)
+         return NULL;
 
+     if (list1->val < list2->val) {
+         root->val = list1->val;
+         list1 = list1->next; 
+     }
+     else {
+         root->val = list2->val;
+         list2 = list2->next;
+     }
 
-	if (list1->val < list2->val) {
-		root->val = list1->val;
-        list1 = list2->next;
-	}
-	else {
-		root->val = list2->val;
-        list2 = list2->next;
-	}
+     while (list1 != NULL && list2 != NULL) {
+         iter->next = (node_t*)malloc(sizeof(node_t));
+         iter = iter->next;
+         if (list1->val > list2->val) {
+             iter->val = list2->val;
+             list2 = list2->next;
+         }
+         else {
+         if (iter == NULL) {
+			 fprintf(stderr, "Memory allocation failed\n");
+			 exit(EXIT_FAILURE);
+         }
+             iter->val = list1->val;
+             list1 = list1->next;
+         }
 
+     }
 
-    while (list1 != NULL && list2 != NULL) {
-        iter->next = (node_t*)malloc(sizeof(node_t));
-        iter = iter->next;
-        if (list1->val > list2->val) {
-            iter->val = list2->val;
-            list2 = list2->next;
-        }
-        else {
-            iter->val = list1->val;
-            list1 = list1->next;
-        }
-    }
-	if (list1 == NULL && list2 != NULL) {
-		iter->next = (node_t*)malloc(sizeof(node_t));
-		iter = iter->next;
-		iter->val = list2->val;
-		list2 = list2->next;
-	}
-	else if (list1 != NULL && list2 == NULL) {
-		iter->next = (node_t*)malloc(sizeof(node_t));
-		iter = iter->next;
-		iter->val = list1->val;
-		list1 = list1->next;
-	}
-    iter->next = NULL;
-    return root;
-}
+     while (list2 != NULL) {
+         iter->next = (node_t*)malloc(sizeof(node_t));
+         iter = iter->next;
+         if (iter == NULL) {
+			 fprintf(stderr, "Memory allocation failed\n");
+			 exit(EXIT_FAILURE);
+         }
+         iter->val = list2->val;
+         list2 = list2->next;
+     }
+
+     while (list1 != NULL) {
+         iter->next = (node_t*)malloc(sizeof(node_t));
+         iter = iter->next;
+         if (iter == NULL) {
+			 fprintf(stderr, "Memory allocation failed\n");
+			 exit(EXIT_FAILURE);
+         }
+         iter->val = list1->val;
+         list1 = list1->next;
+     }
+
+     iter->next = NULL;
+     return root;
+ }
 
 int main(int argc, char** argv) {
 
-    int val[] = { 1,2,3 };
-    node_t* l1 = init_node(3, val);
+    int val1[] = { 1,2,3 };
+    int val2[] = { 2,3,4 };
+    node_t* l1 = init_node(3, val1);
+    node_t* l2 = init_node(3, val2);
     print_list_node(l1);
-    push_node(l1, 4);
+    push_node(&l1, 4);
     print_list_node(l1);
-
+    push_at_beginning(&l1, 0);
+    print_list_node(l1);
+    node_t* l3 = mergeTwoLists(l1, l2);
+    print_list_node(l3);
 	return 0;
 }
